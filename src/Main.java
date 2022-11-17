a49491731811fa77567d6fc5bcf4be94ed5d50ae
Yunus Emre Eren <113580478+Emreren11@users.noreply.github.com>
2022-11-17 17:00:58 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Emre", 2000, 45, 1985);
+        System.out.println(emp); // toString komutunu çalıştırır
+    }
+}
